25a5bcf3b3b46b010d7b9e4b3a51c15d8a372122
FpSweeper <123176236+fpsweeper@users.noreply.github.com>
2023-02-04 17:42:22 +0100

    Update app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,16 +4,9 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import {NftsComponent} from './nfts/nfts.component';
-import {CollectionsComponent} from './collections/collections.component';
-import { MarketplacesComponent } from './marketplaces/marketplaces.component';
+
 const routes: Routes =[
     { path: 'home',             component: HomeComponent },
-    { path: 'nfts',             component: NftsComponent },
-    { path: 'nfts/:mint',             component: NftsComponent },
-    { path: 'collections',             component: CollectionsComponent },
-    { path: 'collections/:fvc',             component: CollectionsComponent },
-    { path: 'marketplaces/:marketplace',             component: MarketplacesComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
